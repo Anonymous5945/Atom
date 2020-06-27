b39887040ae71a3c54abf0cb3da7a8eceadf968b
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-06-27 08:27:07 +0530

    Create __init__.py

---
+++ Atom/__init__.py
@@ -0,0 +1,27 @@
+import os
+
+# the secret configuration specific things
+if bool(os.environ.get("ENV", False)):
+    from tobrot.sample_config import Config
+else:
+    from tobrot.config import Config
+
+
+# TODO: is there a better way?
+TG_BOT_TOKEN = Config.TG_BOT_TOKEN
+APP_ID = Config.APP_ID
+API_HASH = Config.API_HASH
+AUTH_CHANNEL = Config.AUTH_CHANNEL
+DOWNLOAD_LOCATION = Config.DOWNLOAD_LOCATION
+EXEC_CMD_TRIGGER = Config.EXEC_CMD_TRIGGER
+MAX_FILE_SIZE = Config.MAX_FILE_SIZE
+TG_MAX_FILE_SIZE = Config.TG_MAX_FILE_SIZE
+FREE_USER_MAX_FILE_SIZE = Config.FREE_USER_MAX_FILE_SIZE
+CHUNK_SIZE = Config.CHUNK_SIZE
+MAX_MESSAGE_LENGTH = Config.MAX_MESSAGE_LENGTH
+PROCESS_MAX_TIMEOUT = Config.PROCESS_MAX_TIMEOUT
+EDIT_SLEEP_TIME_OUT = Config.EDIT_SLEEP_TIME_OUT
+MAX_TG_SPLIT_FILE_SIZE = Config.MAX_TG_SPLIT_FILE_SIZE
+Eval_CMD_TRIGGER = Config.Eval_CMD_TRIGGER
+Save_CMD_TRIGGER = Config.Save_CMD_TRIGGER
+
