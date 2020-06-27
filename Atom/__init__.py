3a0f2befe1ae241eb2ff3caf80d489fb5438e0c5
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-06-27 08:47:09 +0530

    Update __init__.py

--- Atom/__init__.py
+++ Atom/__init__.py
@@ -2,9 +2,9 @@ import os
 
 # the secret configuration specific things
 if bool(os.environ.get("ENV", False)):
-    from tobrot.sample_config import Config
+    from Atom.sample_config import Config
 else:
-    from tobrot.config import Config
+    from Atom.config import Config
 
 
 # TODO: is there a better way?
