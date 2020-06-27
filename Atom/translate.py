6dfe3a0b7e753c892bede1d43714682023ec7a02
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-06-27 09:21:44 +0530

    Update translate.py

--- Atom/translate.py
+++ Atom/translate.py
@@ -6,6 +6,13 @@ logging.basicConfig(
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
 
+import io
+import asyncio
+import inspect
+import os
+import time
+import sys
+import traceback
 
 """ Google Translate
 Available Commands:
